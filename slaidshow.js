1bc38fa8686d45b51daee6628680ea714bae5537
Kadi-Riin Toomoja <riintoom@ut.ee>
2020-11-30 17:28:04 +0200

    JS kommentaarid

--- slaidshow.js
+++ slaidshow.js
@@ -1,28 +1,32 @@
 // Kasutatud material slaidshowks: 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
-var slideIndex = 1;
+var slideIndex = 1; //paeb slaidi indeks väärtuseks ühe
 showSlides(slideIndex);
 
-function plusSlides(n) {
+function plusSlides(n) //paneb slaidi indeksi väärtuseks järgmise slaidi väärtuse
+{
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function currentSlide(n) //paneb slaidi indeksi väärtuseks esitatava slaidi väärtuse
+{ 
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  var i; //muutuja nimi
+  var slides = document.getElementsByClassName("mySlides"); //loeb dokumendist kokku piltide arvu ja seab selle muutuja slides väärtuseks
+  var dots = document.getElementsByClassName("dot"); //loeb dokumendist kokku nö täppide arvu ja seab selle vastava muutuja väärtuseks
+  if (n > slides.length) {slideIndex = 1}   //kui parameetri n väärtus läheb suuremaks kui slaidide arv, paneb slaidide indeksi väärtuseks ühe
+  if (n < 1) {slideIndex = slides.length} //kui parameetri n väärtus on väiksem kui üks seab slaidi indeksi väärtuseks suurima slaidi arvu
+  for (i = 0; i < slides.length; i++) //Suurendab muutujat i iga kord ühe võrra kui i on väiksem kui slaidide koguarv
+  {
+      slides[i].style.display = "none";  //vastutab selle eest, et esitataks vaid ühte pilti korraga
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dots.length; i++) //suurenab muutuja i iga kord ühe võrra kui i väärtus väiksem kui täppide koguarv
+  {
+      dots[i].className = dots[i].className.replace(" active", ""); //vastutab, et täpid, mis ei tohiks olla aktiivsed seda ka ei oleks
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block";  //esitab õiget slaidi
+  dots[slideIndex-1].className += " active"; //Valitud järiekorra numbriga täpp muutub aktiivseks
 }
